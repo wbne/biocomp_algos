ee36c564ca51e5b7752f7c1594457d9955a8f7a8
Ben Wang <firetruckbw@gmail.com>
2021-08-22 23:11:25 -0500

    Add Smith-Waterman Algorithm
    Include new comments for N-W as well

--- SmithWaterman.java
+++ SmithWaterman.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-public class NeedlemanWunsch
+public class SmithWaterman
 {
     public static void main(String []args)
     {
         Scanner sc = new Scanner(System.in);
         //Sequences retrieved from the user input
-        String sequence1 = sc.nextLine();
-        String sequence2 = sc.nextLine();
+        //String sequence1 = sc.nextLine();
+        //String sequence2 = sc.nextLine();
         //Sequences hardcoded for testing purposes
-        //String sequence1 = "atttaa"; 
-        //String sequence2 = "attcaga";
-        
+        String sequence1 = "atttaaccgtac"; 
+        String sequence2 = "gctaccatggaggcta";
+
         //The two sequences will be compared to each other via a grid/2d array.
         char[] seq1 = sequence1.toCharArray();
         char[] seq2 = sequence2.toCharArray();
         int[][] map = new int[seq2.length + 1][seq1.length + 1];
         String[][] indices = new String[seq2.length + 1][seq1.length + 1];
-        
-        //The Needleman-Wunsch algorithm serves to completely align two sequences.
-        //During the alignment it also introduces gaps to help make sure it does not catch on fire
-        //The alignment itself is a refinement of the dot-plot alignment
-        
+
+        //The Smith-Waterman algorithm tries to pair the largest common segment together.
+        //The returned pair starts at the first positive valued pair and ends at the highest valued pair.
+        //This allows the pairing to be more accurate and is the successor to the Needleman-Wunsch algorithm.
+
         //These values are used to calculate the score of the specific alignment.
         //The rules of how they are calculated will be explained later.
         int match = 2;
@@ -39,13 +39,13 @@ public class NeedlemanWunsch
             for(int j = 1; j < map[i].length; j++)
             {
                 /*
-                 * When generating the subsequent values of the map, NW uses dynamic programming
+                 * When generating the subsequent values of the map, SW uses dynamic programming
                  * The value at [i,j] is determined by the highest value from 3 options
                  *  1) Going diagonal which means no gap needs to be inserted. 
                  *     The added score is simply its match/mismatch score.
                  *  2) Adding a gap to strand 1 which adds a gap penalty but nothing else.
                  *  3) Adding a gap to strand 2 which also adds a gap penalty.
-                */
+                 */
                 int option = 1;
                 int max = map[i - 1][j - 1];
                 if(seq1[j - 1] == seq2[i - 1])
@@ -82,35 +82,54 @@ public class NeedlemanWunsch
 
         //Creating the "optimal" sequence by traversing backwards
         int length1 = sequence1.length(), length2 = sequence2.length();
-        
+
         //The current X and Y are the ends because NW tries to pair the entire sequence.
         //If you want to only stop at the max value, then that's Smith-Waterman
         int currentX = length1, currentY = length2;
+        int maxValue = -1;
+        
+        //Finds the point with the highest score and sets the start to there.
+        for(int i = 1; i <= length1; i++) {
+            for(int j = 1; j <= length2; j++) {
+                if(map[j][i] > maxValue) {
+                    maxValue = map[j][i];
+                    String location = indices[j][i];
+                    String[] temp = location.split(":");
+                    currentX = Integer.parseInt(temp[0]);
+                    currentY = Integer.parseInt(temp[1]);
+                }
+            }
+        }
         
         int oldX = -1, oldY = -1;
-        String aligned1 = "" + sequence1.charAt(length1 - 1);
-        String aligned2 = "" + sequence2.charAt(length2 - 1);
+        String aligned1 = "";
+        String aligned2 = "";
+        String alignedValue = "";
 
         while(oldX != 0 && oldY != 0) //while not at the end of the plot
         {
-            String location = indices[currentY][currentX];
-            String[] temp = location.split(":");
-            currentX = Integer.parseInt(temp[0]);
-            currentY = Integer.parseInt(temp[1]);
+            //if the prevous and current X locations are the same, there's a gap
             if(oldX != -1)
-                //if the previous and current X locations are the same, there's a gap
                 if(currentX != oldX)
-                    aligned1 += sequence1.charAt(oldX - 1);
+                    aligned1 += sequence1.charAt(oldX);
                 else
                     aligned1 += "-";
+            //same idea but with the Y location and sequence 2
             if(oldY != -1)
-                //same idea but with the Y location and sequence 2
-                if(currentY != oldY)
-                    aligned2 += sequence2.charAt(oldY - 1);
+                if(currentY != oldY && oldY != -1)
+                    aligned2 += sequence2.charAt(oldY);
                 else
                     aligned2 += "-";
+
+            //checks the value and if it is zero or less it exits
+            if(map[currentY][currentX] <= 0)
+                break;
+            String location = indices[currentY][currentX];
+            String[] temp = location.split(":");
             oldX = currentX;
             oldY = currentY;
+            currentX = Integer.parseInt(temp[0]);
+            currentY = Integer.parseInt(temp[1]);
         }
 
         //Prints out the aligned sequence
@@ -123,7 +142,7 @@ public class NeedlemanWunsch
         System.out.println();
 
     }
-    
+
     //Prints out the character grid
     private static void print(int[][] map, String sequence1, String sequence2)
     {
